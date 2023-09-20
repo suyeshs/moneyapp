9f05de6ccbc105957743de3c6147b5e5e082eb08
Suyesh Shankar <suyesh@gmail.com>
2023-09-20 15:06:13 +0530

    removed contexts

--- src/app/contexts/storeContext.tsx
+++
@@ -1,6 +0,0 @@
-// storeContext.ts
-import { createContext } from 'react';
-import { NseFetchStore } from '../../stores/NseFetchStore'; // Update the path accordingly
-
-const store = new NseFetchStore();
-export const StoreContext = createContext(store);
